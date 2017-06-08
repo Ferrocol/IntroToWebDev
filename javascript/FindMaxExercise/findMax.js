73a4c9a0441fadbf13a2ffdf4bf136354ce9e957
Ferrocol <leannerenner@gmail.com>
2017-06-07 22:29:32 -0500

    Exercise - Find Max finished, Exercise - Sum of Range turned in

--- javascript/FindMaxExercise/findMax.js
+++ javascript/FindMaxExercise/findMax.js
@@ -1,8 +1,9 @@
-function findMax() {
+function findMax(numbers) {
     var max = numbers[0];
     for (i = 0; i < numbers.length; i++) {
         if (numbers[i] > max) {
             max = numbers[i];
         }
     }
+    return max;
 }
---
+++ javascript/SumofRangeExercise/sumOfRange.js
@@ -0,0 +1,7 @@
+function sumOfRange(numbers) {
+    var sum = 0;
+    for (i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
